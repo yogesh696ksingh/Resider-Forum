9648b5d6cb9905c1d1baffacaad2795504ee6e6a
yogesh696ksingh <yogesh696ksingh@gmail.com>
2018-03-04 23:42:02 +0530

    fetched data for dashboard complaints components

--- app/Http/Controllers/pageController.php
+++ app/Http/Controllers/pageController.php
@@ -18,7 +18,10 @@ class PageController extends Controller
     public function searchpost(Request $request) {
         $user_id = 1;
         $all_complaints = DB::table('complaint')->where('user_id',$user_id)->get();
-        echo json_encode($all_complaints);
+        $plucked_user_id = $all_complaints->pluck('user_id');
+        $all_user_id = array_unique($plucked_user_id->all());
+        $all_user_info = DB::table('users')->select('id','name')->whereIn('id',$all_user_id)->get();
+        echo json_encode($all_user_info);
     }    
     
 }
