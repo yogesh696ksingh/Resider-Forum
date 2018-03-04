ea885e0823a237ee7bd2759b25fc6752ac786620
yogesh696ksingh <yogesh696ksingh@gmail.com>
2018-03-05 01:55:25 +0530

    searchbar complete

--- app/Http/Controllers/pageController.php
+++ app/Http/Controllers/pageController.php
@@ -10,18 +10,45 @@ class PageController extends Controller
         if(DB::connection()->getDatabaseName())
             {
                 echo "connected successfully to database ".DB::connection()->getDatabaseName();
-                $all_complaints = DB::table('complaint')->where('user_id','1')->get();
-        echo json_encode($all_complaints);
             }
     }
 
     public function searchpost(Request $request) {
-        $user_id = 1;
-        $all_complaints = DB::table('complaint')->where('user_id',$user_id)->get();
+        $user_id = 1; /*session: user_id*/
+        $query = DB::table('complaint');
+        $search_mat = $request->all();
+        foreach ($search_mat as $key => $value) {
+            if ($value["field_id"] == 'freetext') {
+                $all_complaints = $query->where('title','like','%'.$value['value'].'%')->orWhere('description','like','%'.$value['value'].'%');
+            }
+            if ($value["field_id"] == 'location') {
+                $all_complaints = $query->where('location_id','=', $value['value']);
+            }
+            if ($value["field_id"] == 'status') {
+                $all_complaints = $query->where('status',$value['value']);
+            }
+            if ($value["field_id"] == 'user' && $value['value']=='mine') {
+                $all_complaints = $query->where('user_id',$user_id); /*session: user_id*/
+            }
+            
+        }
+      
+        $all_complaints = $query->get();
         $plucked_user_id = $all_complaints->pluck('user_id');
-        $all_user_id = array_unique($plucked_user_id->all());
-        $all_user_info = DB::table('users')->select('id','name')->whereIn('id',$all_user_id)->get();
-        echo json_encode($all_user_info);
+        $plucked_user_id = array_unique($plucked_user_id->all());
+        $all_user_info = DB::table('users')->select('id','name')->whereIn('id',$plucked_user_id)->get();
+        $all_complaints = json_decode(json_encode($all_complaints),true);
+        $user_info = $all_user_info->mapWithKeys(function ($item) {
+            return [$item->id => $item->name];
+        });
+        $user_info = $user_info->all();
+        $user_info = json_decode(json_encode($user_info),true);
+        foreach ($all_complaints as $key => $value) {
+            $all_complaints[$key]['username'] = $user_info[$value['user_id']];
+        }
+        $all_complaints_count = DB::table('complaint')->where('user_id',$user_id)->count();
+        echo json_encode($all_complaints);
     }    
     
+
 }
